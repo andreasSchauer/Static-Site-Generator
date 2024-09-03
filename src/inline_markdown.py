a7f9fc74591b03da94c5fc0ce477eecdab921f8a
andreasSchauer <andreas.schauer@yahoo.com>
2024-09-03 17:31:06 +0200

    H: completed inline conversion

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -4,7 +4,21 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_link,
+    text_type_image
 )
+import re
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    return nodes
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -30,6 +44,79 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 nodes_to_add.append(TextNode(split_text[i], text_type_text))
             else:
                 nodes_to_add.append(TextNode(split_text[i], text_type))
+
+        new_nodes.extend(nodes_to_add)
+
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    image_regex = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(image_regex, text)
+    return matches
+
+def extract_markdown_links(text):
+    link_regex = r"(?<!!)\[(.*?)\]\((.*?)\)"
+    matches = re.findall(link_regex, text)
+    return matches
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        
+        images = extract_markdown_images(node.text)
+
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+
+        split_text = re.split(r"!\[.*?\]\(.*?\)", node.text)
+        nodes_to_add = []
+
+        for i in range(len(split_text)):
+            if split_text[i] != "":
+                nodes_to_add.append(TextNode(split_text[i], text_type_text))
+            
+            if i < len(images):
+                image_alt = images[i][0]
+                image_link = images[i][1]
+                nodes_to_add.append(TextNode(image_alt, text_type_image, image_link))
+
+        new_nodes.extend(nodes_to_add)
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        
+        links = extract_markdown_links(node.text)
+
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+
+        split_text = re.split(r"(?<!!)\[.*?\]\(.*?\)", node.text)
+        nodes_to_add = []
+
+        for i in range(len(split_text)):
+            if split_text[i] != "":
+                nodes_to_add.append(TextNode(split_text[i], text_type_text))
+            
+            if i < len(links):
+                link_text = links[i][0]
+                link_url = links[i][1]
+                nodes_to_add.append(TextNode(link_text, text_type_link, link_url))
 
         new_nodes.extend(nodes_to_add)
 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,11 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    split_nodes_link,
+    split_nodes_image,
+    extract_markdown_links,
+    extract_markdown_images,
+    text_to_textnodes
 )
 
 from textnode import (
@@ -9,6 +14,8 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_image,
+    text_type_link
 )
 
 
@@ -97,6 +104,135 @@ class TestInlineMarkdown(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             split_nodes_delimiter([node], "a", text_type_code)
         self.assertEqual(str(context.exception), "Invalid Markdown Syntax: Delimiter not supported")
+
+    def test_links_with_match(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual(
+            [
+                ('to boot dev', 'https://www.boot.dev'),
+                ('to youtube', 'https://www.youtube.com/@bootdotdev')
+            ],
+            matches
+        )
+
+    def test_links_without_match(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([], matches)
+
+    def test_images_with_match(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual(
+            [
+               ('rick roll', 'https://i.imgur.com/aKaOqIh.gif'),
+               ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')
+            ],
+            matches
+        )
+
+    def test_images_without_match(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([], matches)
+
+    def test_split_images_(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg), my guy"
+        node = TextNode(text, text_type_text)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("rick roll", text_type_image, "https://i.imgur.com/aKaOqIh.gif"),
+                TextNode(" and ", text_type_text),
+                TextNode("obi wan", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(", my guy", text_type_text)
+            ],
+            new_nodes
+        )
+
+    def test_split_images_without_text_start_end(self):
+        text = "![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        node = TextNode(text, text_type_text)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("rick roll", text_type_image, "https://i.imgur.com/aKaOqIh.gif"),
+                TextNode(" and ", text_type_text),
+                TextNode("obi wan", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            new_nodes
+        )
+
+    def test_split_images_without_match(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        node = TextNode(text, text_type_text)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", text_type_text)
+            ]
+            , new_nodes
+        )
+
+    def test_split_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev), my guy"
+        node = TextNode(text, text_type_text)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", text_type_text),
+                TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode("to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"),
+                TextNode(", my guy", text_type_text)
+            ],
+            new_nodes
+        )
+
+    def test_split_links_without_text_start_end(self):
+        text = "[to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        node = TextNode(text, text_type_text)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+                TextNode(" and ", text_type_text),
+                TextNode("to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"),
+            ],
+            new_nodes
+        )
+    
+    def test_split_links_without_match(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg), my guy"
+        node = TextNode(text, text_type_text)
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg), my guy", text_type_text)
+            ],
+            new_nodes
+        )
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ],
+            new_nodes
+        )
 
 
 if __name__ == "__main__":
