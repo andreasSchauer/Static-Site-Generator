2c9bd6e04fa3db2132ddb2d10572aece9d828f43
andreasSchauer <andreas.schauer@yahoo.com>
2024-09-06 19:04:02 +0200

    K: finished block to html conversion

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,9 +1,27 @@
+import re
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+from htmlnode import ParentNode, LeafNode
+
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
 block_type_code = "code"
 block_type_quote = "quote"
 block_type_olist = "ordered_list"
 block_type_ulist = "unordered_list"
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+
+    for block in blocks:
+        nodes = block_to_html_node(block)
+        children.append(nodes)
+
+    return ParentNode("div", children)
+
+
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -19,6 +37,7 @@ def markdown_to_blocks(markdown):
     return filtered_blocks
 
 
+
 def block_to_block_type(block):
     lines = block.split("\n")
     
@@ -31,7 +50,6 @@ def block_to_block_type(block):
         or block.startswith("###### ")
     ):
         return block_type_heading
-    
 
     if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
         return block_type_code
@@ -49,3 +67,129 @@ def block_to_block_type(block):
         return block_type_olist
     
     return block_type_paragraph
+
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    
+    if block_type == block_type_paragraph:
+        return paragraph_to_html_nodes(block)
+    
+    if block_type == block_type_heading:
+        return heading_to_html_nodes(block)
+    
+    if block_type == block_type_code:
+        return code_to_html_nodes(block)
+    
+    if block_type == block_type_quote:
+        return quote_to_html_nodes(block)
+    
+    if block_type == block_type_olist:
+        return olist_to_html_nodes(block)
+    
+    if block_type == block_type_ulist:
+        return ulist_to_html_nodes(block)
+
+    raise ValueError("Invalid block type")
+
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+
+    return children
+
+
+
+def paragraph_to_html_nodes(paragraph_block):
+    lines = paragraph_block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+
+def heading_to_html_nodes(heading_block):
+    heading_tag = get_heading_tag(heading_block)
+    clean_text = heading_block.lstrip("# ")
+    children = text_to_children(clean_text)
+    return ParentNode(heading_tag, children)
+
+
+
+def code_to_html_nodes(code_block):
+    clean_text = code_block.strip("`\n")
+    children = text_to_children(clean_text)
+    code_node = ParentNode("code", children)
+    return ParentNode("pre", [code_node])
+
+
+
+def quote_to_html_nodes(quote_block):
+    lines = quote_block.split("\n")
+    clean_lines = []
+    
+    for line in lines:
+        if line == "":
+            continue
+        clean_lines.append(line.strip(">").strip())
+
+    quote = " ".join(clean_lines)
+    children = text_to_children(quote)
+    return ParentNode("blockquote", children)
+
+        
+
+def ulist_to_html_nodes(unordered_list_block):
+    lines = unordered_list_block.split("\n")
+    list_items = []
+    
+    for line in lines:
+        if line == "":
+            continue
+        
+        clean_line = line.lstrip("*- ")
+        children = text_to_children(clean_line)
+        list_items.append(ParentNode("li", children))
+
+    return ParentNode("ul", list_items)
+
+
+
+def olist_to_html_nodes(ordered_list_block):
+    lines = ordered_list_block.split("\n")
+    list_items = []
+    
+    for line in lines:
+        if line == "":
+            continue
+        
+        clean_line = line.lstrip("1234567890. ")
+        children = text_to_children(clean_line)
+        list_items.append(ParentNode("li", children))
+        
+    return ParentNode("ol", list_items)
+
+
+
+
+def get_heading_tag(heading_block):
+    hashtags = re.findall(r"^(#+) ", heading_block)
+    
+    if len(hashtags) == 0:
+        raise ValueError("Input is not a heading block")
+
+    heading_num = len(hashtags[0])
+    heading_tag = f"h{heading_num}"
+    return heading_tag
+
+
+
+
+
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -3,6 +3,8 @@ import textwrap
 from block_markdown import (
     markdown_to_blocks,
     block_to_block_type,
+    get_heading_tag,
+    markdown_to_html_node,
     block_type_paragraph,
     block_type_code,
     block_type_heading,
@@ -67,6 +69,8 @@ class TestBlockMarkdown(unittest.TestCase):
             markdown_blocks
         )
 
+
+
     def test_block_to_block_type_paragraph(self):
         text = "This is a paragraph"
         block_type = block_to_block_type(text)
@@ -110,6 +114,101 @@ class TestBlockMarkdown(unittest.TestCase):
         block_type = block_to_block_type(text)
         self.assertEqual(block_type_olist, block_type)
 
+
+
+    def test_heading_tag_1(self):
+        text = "### heading# sdjfio"
+        tag = get_heading_tag(text)
+        self.assertEqual("h3", tag)
+
+    def test_heading_tag_2(self):
+        text = "##### #heading"
+        tag = get_heading_tag(text)
+        self.assertEqual("h5", tag)
+
+    def test_heading_tag_error(self):
+        text = "heading"
+        with self.assertRaises(ValueError) as context:
+            get_heading_tag(text)
+        self.assertEqual(str(context.exception), "Input is not a heading block")
+
+    
+    
+    def test_paragraph(self):
+        md = textwrap.dedent("""\
+            This is **bolded** paragraph
+            text in a p
+            tag here""")
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = textwrap.dedent("""\
+            This is **bolded** paragraph
+            text in a p
+            tag here
+
+            This is another paragraph with *italic* text and `code` here""")
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_lists(self):
+        md = textwrap.dedent("""
+            - This is a list
+            - with items
+            - and *more* items
+
+            1. This is an `ordered` list
+            2. with items
+            3. and more items""")
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+    def test_headings(self):
+        md = textwrap.dedent("""\
+            # this is an h1
+
+            this is paragraph text
+
+            ## this is an h2""")
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = textwrap.dedent("""
+            > This is a
+            > blockquote block
+
+            this is paragraph text""")
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
+
+    
 
 if __name__ == "__main__":
     unittest.main()
